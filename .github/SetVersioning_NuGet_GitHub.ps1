163f14478ea2162d50bb7d233cc067085404a139
Richard Angus <48549112+rcangus@users.noreply.github.com>
2020-12-15 11:59:40 +1000

    Try different way to set the env variable for use by the pack commad

--- .github/SetVersioning_NuGet_GitHub.ps1
+++ .github/SetVersioning_NuGet_GitHub.ps1
@@ -3,11 +3,16 @@ Write-Host "rev number is " $rev
 
 $branch = $env:GITHUB_REF;
 
+
+Write-Host "::set-env name=fa_name::$($fa.name)"
+
 if($branch -like '*feature*'){
     #get the branch name from the last / 
     $featureName = $branch.Substring($branch.LastIndexOf('/')+1)
     $ver = -join("1.0.", $featureName, ".", $rev)
-    $env:NUGET_VERSION = $ver
+    Write-Host "::set-env name=NUGET_VERSION::$($ver)"
+    #$env:NUGET_VERSION = $ver
 } else {
-    $env:NUGET_VERSION = -join("1.0.", $rev)
+    $ver = -join("1.0.", $rev)
+    Write-Host "::set-env name=NUGET_VERSION::$($ver)"
 }
