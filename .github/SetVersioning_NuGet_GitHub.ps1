a6e2216bfcca9a65ba93fb59ea988e2d2ecb79ad
Richard Angus <48549112+rcangus@users.noreply.github.com>
2020-12-15 12:11:12 +1000

    Change to use new env files

--- .github/SetVersioning_NuGet_GitHub.ps1
+++ .github/SetVersioning_NuGet_GitHub.ps1
@@ -3,16 +3,11 @@ Write-Host "rev number is " $rev
 
 $branch = $env:GITHUB_REF;
 
-
-Write-Host "::set-env name=fa_name::$($fa.name)"
-
 if($branch -like '*feature*'){
     #get the branch name from the last / 
     $featureName = $branch.Substring($branch.LastIndexOf('/')+1)
     $ver = -join("1.0.", $featureName, ".", $rev)
-    Write-Host "::set-env name=NUGET_VERSION::$($ver)"
-    #$env:NUGET_VERSION = $ver
 } else {
     $ver = -join("1.0.", $rev)
-    Write-Host "::set-env name=NUGET_VERSION::$($ver)"
 }
+echo "NUGET_VERSION=$($ver)" >> $GITHUB_ENV
