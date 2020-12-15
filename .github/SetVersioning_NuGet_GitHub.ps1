cb18589f47e6dce2dcaaeab40da8f71bf63380bb
Richard Angus <48549112+rcangus@users.noreply.github.com>
2020-12-15 11:53:31 +1000

    fixed powershell errors

--- .github/SetVersioning_NuGet_GitHub.ps1
+++ .github/SetVersioning_NuGet_GitHub.ps1
@@ -6,7 +6,7 @@ $branch = $env:GITHUB_REF;
 if($branch -like '*feature*'){
     #get the branch name from the last / 
     $featureName = $branch.Substring($branch.LastIndexOf('/')+1)
-    $ver = -join("1.0.", $featureName, $rev
+    $ver = -join("1.0.", $featureName, ".", $rev)
     $env:NUGET_VERSION = $ver
 } else {
     $env:NUGET_VERSION = -join("1.0.", $rev)
