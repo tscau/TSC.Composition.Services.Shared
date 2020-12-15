39db8704b42085182375bd94dba01314088a4ca1
Richard Angus <48549112+rcangus@users.noreply.github.com>
2020-12-15 11:44:36 +1000

    Added PowerShell for versioning the package Added nuget package creation and push to GitHub pacakges

---
+++ .github/SetVersioning_NuGet_GitHub.ps1
@@ -0,0 +1,13 @@
+$rev = $env:GITHUB_RUN_NUMBER
+Write-Host "rev number is " $rev
+
+$branch = $env:GITHUB_REF;
+
+if($branch -like '*feature*'){
+    #get the branch name from the last / 
+    $featureName = $branch.Substring($branch.LastIndexOf('/')+1)4
+    $ver = -join("1.0.", $featureName, $rev
+    $env:NUGET_VERSION = $ver
+} else {
+    $env:NUGET_VERSION = -join("1.0.", $rev)
+}
